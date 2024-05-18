d8e22ba3a0d9f5c0568cac486048e0596e1b1fb7
misaelvillaverde <misaelvilla10@gmail.com>
2024-05-18 18:27:43 -0500

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -13,9 +13,21 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Error writing data to the connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	err = conn.Close()
+	if err != nil {
+		fmt.Println("Error closing connection: ", err.Error())
 		os.Exit(1)
 	}
 }
