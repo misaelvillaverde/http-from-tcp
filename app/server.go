47c640174eea7092c4e84b78afa7166b52addb95
misaelvillaverde <misaelvilla10@gmail.com>
2024-05-18 19:15:00 -0500

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -19,15 +20,34 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	defer func() {
+		err = conn.Close()
+		if err != nil {
+			fmt.Println("Error closing connection: ", err.Error())
+			os.Exit(1)
+		}
+	}()
+
+	request := make([]byte, 50)
+	_, err = conn.Read(request)
 	if err != nil {
-		fmt.Println("Error writing data to the connection: ", err.Error())
+		fmt.Println("Error reading data from the connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	err = conn.Close()
+	parts := strings.Split(string(request), "\r\n")
+
+	requestLine := strings.Split(parts[0], " ")
+
+	switch requestLine[1] {
+	case "/":
+		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	default:
+		_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+
 	if err != nil {
-		fmt.Println("Error closing connection: ", err.Error())
+		fmt.Println("Error writing data to the connection: ", err.Error())
 		os.Exit(1)
 	}
 }
